70011091b027c6deccacf825546a7adf85cd5c2c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 15:09:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,11 @@ def oxford_comma(array)
     return array.join(" and ")
   else
     new_string = ""
-    array.each_with_index{
-      new_string += "#{array[i]}, "
+    array.each_with_index{ |val, index|
+      new_string += "#{val}, "
+      if index == (array.size - 1)
+        new_string + " and " + array[-1]
+      end
     }
-    return new_string + " and " + array[-1]
   end
 end
