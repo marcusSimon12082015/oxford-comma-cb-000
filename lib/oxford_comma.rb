3063e122ecc14a0e44edf3323f7ee9930144fafb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 15:10:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array.each_with_index{ |val, index|
       new_string += "#{val}, "
       if index == (array.size - 1)
-        new_string + " and " + array[-1]
+        new_string += " and " + array[-1]
       end
     }
   end
