a31a31183ac0c90fe44509458cdb8f2a29d42e4f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 15:12:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    new_string = ""
+    array.each_with_index{ |val, index|
+      if index == (array.size - 1)
+        new_string += "and " + array[-1]
+      else
+        new_string += "#{val}, "
+      end
+    }
+  end
+  return new_string
 end
